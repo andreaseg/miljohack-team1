f57ab15001e6b0b6b2aa7777a5b36e72393bd5cd
Andreas Grønlien <andreas@Andreass-MacBook-Pro-1634.local>
2021-11-06 10:34:18 +0100

    Styled FeatureInfo

--- src/components/FeaturesInfo.js
+++ src/components/FeaturesInfo.js
@@ -1,55 +1,67 @@
-import React from 'react';
-import Row from './Row';
-import getKey from '../utils/GetKey';
-import Note from './Note';
+import React from "react";
+import Row from "./Row";
+import getKey from "../utils/GetKey";
+import Note from "./Note";
 
-  const featureNames = {
-    CEILINGS: "Tak",
-    WALLS: "Vegger",
-    FLOORS: "Gulv",
-    WINDOWS: "Vinduer",
-    HEATING_UNIT: "Oppvarming",
-    FRIDGE: "Kjøleskap",
-    WASHING_MACHINE: "Vaskemaskin",
-    CLOTHES_DRYER: "Tørketrommel",
-    SHOWER: "Dusj"
-  }
+import styles from "./FeaturesInfo.module.css";
+
+const featureNames = {
+  CEILINGS: "Tak",
+  WALLS: "Vegger",
+  FLOORS: "Gulv",
+  WINDOWS: "Vinduer",
+  HEATING_UNIT: "Oppvarming",
+  FRIDGE: "Kjøleskap",
+  WASHING_MACHINE: "Vaskemaskin",
+  CLOTHES_DRYER: "Tørketrommel",
+  SHOWER: "Dusj",
+};
 
 class FeaturesInfo extends React.Component {
-
-
   render() {
     const features = this.props.features;
 
     if (features && features.length > 0) {
       const values = this.props.features;
 
-      const sorted = values.sort((a, b) => (a.pollution < b.pollution) ? 1 : -1).slice(0, 3);
-      const sumSorted = sorted.map(item => item.pollution).reduce((acc, a) => acc + a, 0);
+      const sorted = values
+        .sort((a, b) => (a.pollution < b.pollution ? 1 : -1))
+        .slice(0, 3);
 
-      const rows = sorted.map(feature => 
-        (
-          <div key={getKey()}>
-            <h4>{featureNames[feature.type]}</h4>
-            Forbruk: {parseInt(feature.energy)+" kWh"}<br />
-            Utslipp: {parseInt(feature.pollution)+" CO2-ekvivalenter"}<br />
-            Utgifter: {parseInt(feature.expense)+"kr"}<br />
-          </div>)
-      )
-
+      const rows = sorted.map((feature) => (
+        <div key={getKey()}>
+          <div className={styles.header}>{featureNames[feature.type]}</div>
+          <div className={styles.container}>
+            <div className={styles.energy}>
+              <div className={styles.value}>{parseInt(feature.energy)} KwT</div>
+              <div className={styles.label}>Forbruk</div>
+            </div>
+            <div className={styles.pollution}>
+              <div className={styles.value}>{parseInt(feature.expense)} kr</div>
+              <div className={styles.label}>Utgifter</div>
+            </div>
+            <div className={styles.expenses}>
+              <div className={styles.value}>
+                {parseInt(feature.pollution)} CO<sub>2</sub>
+              </div>
+              <div className={styles.label}>Utslipp</div>
+            </div>
+          </div>
+        </div>
+      ));
 
       return (
-          <div>
-            <h2>Ditt forbruk</h2>
-              Ditt totale forbruk er {parseInt(sumSorted)}<br />
-              Se hvilke kråker som stjeler mest!
-            <h3>Dine top 3 kråker!</h3>
-            {rows}
-
-          </div>
-      )
+        <div>
+          <h2>Ditt forbruk</h2>
+          <Note>
+            <h2>Dine top 3 kråker!</h2>
+            <p>Vår energikråke har sett på huset ditt og funnet topp tre plasser å stå.</p>
+          </Note>
+          {rows}
+        </div>
+      );
     } else {
-      return (null)
+      return null;
     }
   }
 }
