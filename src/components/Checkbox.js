c3cbccfd2609e87ca41a9a070f31aa61cb2920a8
Andreas Grønlien <andreas@Andreass-MacBook-Pro-1634.local>
2021-11-06 12:38:51 +0100

    Fixed screenreader labels

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -15,10 +15,14 @@ const Checkbox = (props) => {
 
   const onKeyPressHandle = (event) => {
     console.log(event.keyCode);
-    const isEnterOrSpace = event.keyCode === 32 || event.keyCode === 13 || event.keyCode === 0;
+    const isEnterOrSpace =
+      event.keyCode === 32 || event.keyCode === 13 || event.keyCode === 0;
     if (isEnterOrSpace) {
       event.preventDefault();
-      setIsChecked(prev => !prev);
+      setIsChecked((prev) => {
+        props.onCheck(!prev);
+        return !prev;
+      });
     }
   };
 
@@ -32,7 +36,11 @@ const Checkbox = (props) => {
             onChange={onChangeHandle}
             id={id}
           />
-          <label htmlFor={id} tabIndex="0" onKeyPress={onKeyPressHandle}></label>
+          <label
+            htmlFor={id}
+            tabIndex="0"
+            onKeyPress={onKeyPressHandle}
+          ></label>
         </div>
       </div>
       {props.children && <div className={styles.label}>{props.children}</div>}
